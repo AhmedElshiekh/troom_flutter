dfd238b1c4c98780f7e7e7a6744c1ec8bb5415a3
karim <k.adnan.arabi@live.com>
2021-07-28 15:23:10 +0200

    V2

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
+//import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:troom/Pages.dart';
 import 'package:troom/Util/ConstStyles.dart';
@@ -20,14 +20,16 @@ void main() async{
 }
 
 class MyApp extends StatelessWidget {
-  GlobalKey _scaffoldkey = GlobalKey();
+  // GlobalKey _scaffoldkey = GlobalKey();
   @override
+
   Widget build(BuildContext context) {
+
     return GetMaterialApp(
-      key: _scaffoldkey,
+      // key: _scaffoldkey,
       debugShowCheckedModeBanner: false,
       defaultTransition: Transition.downToUp,
-      title: 'TROOM',
+      title: 'Troom',
       theme: ThemeData(
         backgroundColor: ConstStyles.WhiteColor,
         appBarTheme:AppBarTheme(
@@ -37,10 +39,11 @@ class MyApp extends StatelessWidget {
         ),
       ),
       translations: Translation(),
-      // textDirection: TextDirection.rtl,
-      locale:Locale('ar', 'EG'),
-      initialRoute: Home.Id,
-      getPages: Pages.routes,
+      textDirection: TextDirection.rtl,
+      locale:Locale('AR', 'EG'),
+
+     initialRoute: Home.Id,
+     getPages: Pages.routes,
     );
   }
 
